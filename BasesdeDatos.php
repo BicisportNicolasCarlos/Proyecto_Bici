f634723c03e0bfd6e8c0fee994870d3935cc5570
Nicolas <nicolastapiasegovia@gmail.com>
2017-10-19 17:58:29 +0200

    Formulario2

--- BasesdeDatos.php
+++ BasesdeDatos.php
@@ -1,39 +1,49 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title></title>
+		<title>queso</title>
 		<meta charset="utf-8"/>
 	</head>
 	<body>
 		<?php
-			$conexion=mysqli_connect("localhost", "root", "proyecto1_bis", "1718_proyecte_1.sql");
-			if(!$conexion){
-			    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
-			    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
-			    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
-			    exit;
+			$conexion=mysqli_connect("localhost", "root", "", "proyecto1_bis");
+
+
+			if($_REQUEST['ubi']!=""){
+					$q = "SELECT * FROM anunci WHERE anu_ubicacio_robatori LIKE '%$_REQUEST[ubi]%' AND anu_marca LIKE '%$_REQUEST[marca]%' AND anu_color LIKE '$_REQUEST[color]%' AND anu_data_robatori LIKE '%$_REQUEST[data]%'";
 			} else {
-				//informamos a la BD que toda consulta que realicemos, la queremos con los contenidos pasados a utf8
-				$acentos=mysqli_query($conexion, "SET NAMES 'utf8'");
-
-				$categoria=$_REQUEST['cat_id'];
-				$q = "select * from producto where cat_id=$categoria";
-				echo $q . "<br/><br/>";
-				$resultados = mysqli_query($conexion, $q);
-
-				if(mysqli_num_rows($resultados)>0){
-					while($producto = mysqli_fetch_array($resultados)){
-						echo "ID: $producto[pro_id]<br/>";
-						echo "Nombre: $producto[pro_nombre]<br/>";
-						echo "Descripción: $producto[pro_descripcion]<br/>";
-						echo "Precio: $producto[pro_precio]<br/><br/>";
-					}
-				} else {
-					echo "No hay productos que mostrar";
-				}
+					$q = "SELECT * FROM anunci";
 			}
+
+			echo "$q<br/>";
+
+			$consulta = mysqli_query($conexion, $q);
+
+			if(mysqli_num_rows($consulta)){
+				$a_anunci = mysqli_num_rows($consulta);
+				echo "Ubicacion: $_REQUEST[ubi]<br/><br/>";
+				echo "Marca: $_REQUEST[marca]<br/><br/>";
+				echo "Color: $_REQUEST[color]<br/><br/>";
+				echo "Fecha robo: $_REQUEST[data]<br/><br/>";
+				echo "mostrar $a_anunci<br/><br/>";
+
+				while($anunci=mysqli_fetch_array($consulta)){
+					echo "<div style='width: 200px; float: left;'>";
+					echo "<b>ID</b>: $anunci[anu_id]<br/>";
+					echo "<b>Ubicacion</b>: $anunci[anu_ubicacio_robatori]<br/>";
+					echo "<b>Marca</b>: $anunci[anu_marca]<br/>";
+					echo "<b>Color</b>: $anunci[anu_color]<br/>";
+					echo "<b>Fecha robo</b>: $anunci[anu_data_robatori]<br/>";
+					echo "</div>";
+				}
+
+			} else {
+				echo "No hay bicicletas que mostrar";
+			}
+
+
 		?>
+		<br/><br/>
+		<a href="robadas.html">Volver</a>
 	</body>
 </html>
-
-
